9920771fd5c081547d3cd465cab7db814067f7e5
MB Hardwick <48660923+mbhardwick@users.noreply.github.com>
2019-10-30 11:48:44 -0700

    inserted data into db

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Plain');
+INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Special');
+INSERT INTO burgers (burger_name) VALUES ('Deluxe');
